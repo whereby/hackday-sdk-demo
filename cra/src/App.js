39d0c32382ad2436291a8a0810c659188e62b371
James Dooley <james.dooley@whereby.com>
2023-04-24 14:24:20 +0100

    Add CRA

---
+++ cra/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+
+import {
+  useLocalMedia,
+  VideoView,
+  useRoomConnection,
+} from "@whereby.com/browser-sdk";
+
+const WHEREBY_ROOM =
+  "https://funtimes.whereby.com/quiz-app8b84481f-02ad-41d3-86dd-05c537e00ed9";
+
+const MyPreCallUX = ({ localMedia }) => {
+  const { currentCameraDeviceId, cameraDevices, localStream } =
+    localMedia.state;
+  const { setCameraDevice, toggleCameraEnabled } = localMedia.actions;
+
+  return (
+    <div className="preCallView">
+      {/* Render any UI, making use of state */}
+      {cameraDevices.map((d) => (
+        <p
+          key={d.deviceId}
+          onClick={() => {
+            if (d.deviceId !== currentCameraDeviceId) {
+              setCameraDevice(d.deviceId);
+            }
+          }}
+        >
+          {d.label}
+        </p>
+      ))}
+      <VideoView muted stream={localStream} />
+    </div>
+  );
+};
+
+const MyVideoCall = ({ localMedia }) => {
+  const { state, actions, components } = useRoomConnection(WHEREBY_ROOM, {
+    localMedia,
+  });
+
+  const { connectionState, remoteParticipants } = state;
+  const { toggleCamera, toggleMicrophone } = actions;
+  const { VideoView } = components;
+
+  console.log(connectionState);
+  return (
+    <div className="videoGrid">
+      {/* Render any UI, making use of state */}
+      {remoteParticipants.map((p) => (
+        <VideoView key={p.id} stream={p.stream} />
+      ))}
+    </div>
+  );
+};
+
+function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const localMedia = useLocalMedia({ audio: false, video: true });
+
+  return (
+    <div className="App">
+      <main>
+        <button onClick={() => setIsConnected(true)}>All good!</button>
+        {isConnected ? (
+          <MyVideoCall localMedia={localMedia} />
+        ) : (
+          <MyPreCallUX localMedia={localMedia} />
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useLocalMedia, VideoView } from "@whereby.com/browser-sdk";
+
+import reactLogo from "./assets/react.svg";
+
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const WHEREBY_ROOM =
+  "https://funtimes.whereby.com/quiz-app8b84481f-02ad-41d3-86dd-05c537e00ed9";
+
+const PreCall = () => {
+  const localMedia = useLocalMedia({ audio: false, video: true });
+
+  // console.log(localMedia);
+
+  // const { currentCameraDeviceId, cameraDevices, localStream } =
+  // localMedia.state;
+  // const { setCameraDevice, toggleCameraEnabled } = localMedia.actions;
+  // const { VideoView } = components;
+
+  // const { components } = VideoView;
+
+  return (
+    <div className="preCallView">
+      hehe
+      {/* Render any UI, making use of state
+      {cameraDevices.map((d) => (
+        <p
+          key={d.deviceId}
+          onClick={() => {
+            if (d.deviceId !== currentCameraDeviceId) {
+              setCameraDevice(d.deviceId);
+            }
+          }}
+        >
+          {d.label}
+        </p>
+      ))}
+      <VideoView muted stream={localStream} /> */}
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +68,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
