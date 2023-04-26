f1464d08431d6d726c69bf7fc2aecefd8791d7d0
Håvard Holvik <havard@whereby.com>
2023-04-26 14:48:26 +0200

    Render reveal state

--- frontend/src/views/QuestionView/index.tsx
+++ frontend/src/views/QuestionView/index.tsx
@@ -1,8 +1,4 @@
-import {
-  Box,
-  Flex,
-  Wrap,
-} from "@chakra-ui/react";
+import { Box, Flex, Wrap } from "@chakra-ui/react";
 import AnswerCard from "../../components/AnswerCard";
 import QuestionCard from "../../components/QuestionCard";
 import VideoTile from "../../components/VideoTile";
@@ -10,17 +6,33 @@ import { GameActions, GameState } from "../../useQuizGame";
 import { useRoomConnection } from "@whereby.com/browser-sdk";
 type RoomConnectionRef = ReturnType<typeof useRoomConnection>;
 
-export default function QuestionView({ quizState, localMedia, roomConnection, quizActions }: {quizState: GameState, localMedia: any, roomConnection: RoomConnectionRef, quizActions: GameActions} ) {
-
+export default function QuestionView({
+  quizState,
+  localMedia,
+  roomConnection,
+  quizActions,
+}: {
+  quizState: GameState;
+  localMedia: any;
+  roomConnection: RoomConnectionRef;
+  quizActions: GameActions;
+}) {
   const { state } = roomConnection;
-  const {remoteParticipants } = state;
+  const { remoteParticipants } = state;
   const { localStream } = localMedia.state;
-  
+
+  const shouldReveal = quizState.revealAnswers;
   const participantId = roomConnection.state.localParticipant?.id || "unknown";
-  const hasAnswered = !!(quizState.currentAnswers && quizState.currentAnswers[participantId])
+  const hasAnswered = !!(
+    quizState.currentAnswers && quizState.currentAnswers[participantId]
+  );
+
+  if (shouldReveal) {
+    return <p>Reveal answers!</p>;
+  }
 
   if (hasAnswered) {
-    return <p>Answered!</p>
+    return <p>Answered!</p>;
   }
 
   return (
@@ -38,20 +50,29 @@ export default function QuestionView({ quizState, localMedia, roomConnection, qu
       <Wrap spacing="50px" justify="space-evenly" direction={"row"}>
         {Object.keys(quizState.currentQuestion?.alternatives || {}).map((k) => {
           return (
-            <AnswerCard key={k}
+            <AnswerCard
+              key={k}
               locked={hasAnswered}
-              answerText={quizState.currentQuestion?.alternatives[k]} onSelected={()=> quizActions.postAnswer(k)}
+              answerText={quizState.currentQuestion?.alternatives[k]}
+              onSelected={() => quizActions.postAnswer(k)}
             ></AnswerCard>
           );
         })}
       </Wrap>
 
-      <Flex alignItems="center"  justifyContent="center">
-      <VideoTile stream={localStream} isAnsweredBadge={hasAnswered}/>
+      <Flex alignItems="center" justifyContent="center">
+        <VideoTile stream={localStream} isAnsweredBadge={hasAnswered} />
         {remoteParticipants.map((p) => {
           const { id, stream, displayName } = p;
-          const hasParticipantAnswered = !!(quizState.currentAnswers || {})[id]
-          return <VideoTile key={id} stream={stream} name={displayName} isAnsweredBadge={hasParticipantAnswered} />;
+          const hasParticipantAnswered = !!(quizState.currentAnswers || {})[id];
+          return (
+            <VideoTile
+              key={id}
+              stream={stream}
+              name={displayName}
+              isAnsweredBadge={hasParticipantAnswered}
+            />
+          );
         })}
       </Flex>
     </Box>
