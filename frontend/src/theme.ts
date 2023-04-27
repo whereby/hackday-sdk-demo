11701a52e4d002efa8450ff4ad8a7e716019131f
Sarah Rose Ferris <sarahrose.ferris@whereby.com>
2023-04-27 15:53:16 +0100

    Updating text

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -8,7 +8,7 @@ const config: ThemeConfig = {
 const overrides = {
   fonts: {
     heading: `'Bangers', cursive`,
-    // body: `'Nunito', sans`,
+    body: `'Bangers', cursive`,
   },
   components: {
     Heading: {
