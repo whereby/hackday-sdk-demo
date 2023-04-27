c99317462e929a06e0dd8a2adaa8b53eac414bc8
Håvard Holvik <havard@whereby.com>
2023-04-27 14:39:23 +0200

    Add simple device controls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, position } from "@chakra-ui/react";
 
 import { useLocalMedia } from "@whereby.com/browser-sdk";
 import PreCallView from "./views/PreCallView";
 import Game from "./views/Game";
 
 import "./App.css";
+import DeviceControls from "./views/DeviceControls";
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
   const [name, setName] = useState("");
   const localMedia = useLocalMedia({ audio: false, video: true });
+
+  const { localStream } = localMedia.state;
+  const { toggleCameraEnabled, toggleMicrophoneEnabled } = localMedia.actions;
 
   return (
     <Box h="100%" textAlign="center">
@@ -24,6 +28,14 @@ function App() {
         />
       ) : (
         <Game localMedia={localMedia} name={name} />
+      )}
+      {localStream && (
+        <DeviceControls
+          floating={isConnected}
+          toggleCameraEnabled={toggleCameraEnabled}
+          toggleMicrophoneEnabled={toggleMicrophoneEnabled}
+          localStream={localStream}
+        />
       )}
     </Box>
   );
