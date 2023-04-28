08f6f38b51b8f26509998641b04fee608ee91e59
Håvard Holvik <havard@whereby.com>
2023-04-28 10:46:47 +0200

    Clean up scoreboard

--- frontend/src/views/Scoreboard/index.tsx
+++ frontend/src/views/Scoreboard/index.tsx
@@ -15,27 +15,7 @@ export default function Scoreboard({
   quizState,
   roomConnection,
   style,
-  variant,
 }: ScoreboardProps) {
-  const { scores } = quizState;
-  const { localParticipant, remoteParticipants } = roomConnection.state;
-
-  if (!localParticipant) {
-    return <div>Error! Missing local participant</div>;
-  }
-
-  const allParticipants = [localParticipant, ...remoteParticipants];
-
-  const scoreboard = allParticipants
-    .map((p) => {
-      return {
-        participantId: p.id,
-        participantName: p.displayName || "Unknown quizzer",
-        score: scores[p.id] || 0,
-      };
-    })
-    .sort((a, b) => b.score - a.score);
-
   return (
     <div style={style}>
       <Heading my="8">Final Scores</Heading>
@@ -49,11 +29,6 @@ export default function Scoreboard({
           />
         </Box>
       </motion.div>
-      {scoreboard.map((s) => (
-        <motion.h1 key={s.participantId} layout>
-          {s.score} - {s.participantName}
-        </motion.h1>
-      ))}
     </div>
   );
 }
